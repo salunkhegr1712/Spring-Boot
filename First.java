764aca9ddabf5a0eac909f3dd11ffda325d7b521
unknown <salunkhegr19.comp@coep.ac.in>
2023-06-21 00:02:55 +0530

    added few commits on restcontroller

---
+++ First.java
@@ -0,0 +1,12 @@
+// importing basic functionalities from the java library
+import java.lang.*;
+import java.util.*;
+
+// the main class
+public class First {
+
+    // main function of our java code
+    public static void main(String args[]){
+        System.out.println("hello world");
+    }
+}
